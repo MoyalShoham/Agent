d2fd6638d132bbff178e61f11fcbce7ad1684e77
Shoham Moyal <moyal.shoham@gmail.com>
2025-08-07 00:09:25 +0300

    starting with IG

--- tools.py
+++ tools.py
@@ -1,12 +1,33 @@
 from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
 from langchain_community.utilities import WikipediaAPIWrapper
+from langchain_google_community import GoogleSearchRun, GoogleSearchAPIWrapper
 from langchain.tools import Tool
 from datetime import datetime
+from dotenv import load_dotenv
+import requests
 
-search = DuckDuckGoSearchRun()
+
+load_dotenv()
+
+search = GoogleSearchRun(api_wrapper=GoogleSearchAPIWrapper())
 
 search_tool = Tool(
     name="search_web",
     func=search.run,
     description="search the web for information",
+)
+
+def get_instagram_profile():
+    url = f"https://graph.instagram.com/{INSTAGRAM_USER_ID}"
+    params = {
+        "fields": "id,username,media_count,account_type",
+        "access_token": INSTAGRAM_ACCESS_TOKEN
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+instagram_tool = Tool(
+    name="instagram_profile",
+    func=lambda _: get_instagram_profile(),
+    description="Get Instagram profile information for the authenticated user."
 )
